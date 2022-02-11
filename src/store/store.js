f9b65486f3f931f51034da6f35c0783314761710
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-02-11 19:10:17 +0500

    store file updated for query boilerplate

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import testReducer from "./testSlice";
 import { apiSlice } from "./apiSlice";
 
 export default configureStore({
   reducer: {
-    products: testReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
