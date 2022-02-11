1e678beef01e4ee5d51c00fda9fb9c91ba114cf1
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-02-11 19:14:49 +0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchPosts, addNewPost } from "./store/testSlice";
 import {
   useGetProductsQuery,
   useAddNewProductMutation,
